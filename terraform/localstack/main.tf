5a7a64dbee3289a27ab10d33b72d6e4c0f15bb47
iamrosada0 <luisrosada@outlook.com>
2025-05-16 17:52:06 +0100

    fix: add rule to put and get object into s3

--- terraform/localstack/main.tf
+++ terraform/localstack/main.tf
@@ -87,7 +87,12 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# IAM Policy for Lambda
+# S3 Bucket
+resource "aws_s3_bucket" "photo_bucket" {
+  bucket = "telemetry-photos"
+}
+
+# Update IAM Policy
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = aws_iam_role.lambda_role.id
@@ -106,6 +111,11 @@ resource "aws_iam_role_policy" "lambda_policy" {
       },
       {
         Effect   = "Allow"
+        Action   = ["s3:PutObject", "s3:GetObject"]
+        Resource = "${aws_s3_bucket.photo_bucket.arn}/*"
+      },
+      {
+        Effect   = "Allow"
         Action   = ["rekognition:CompareFaces"]
         Resource = "*"
       },
@@ -117,7 +127,6 @@ resource "aws_iam_role_policy" "lambda_policy" {
     ]
   })
 }
-
 # Processor Lambda
 resource "aws_lambda_function" "processor_lambda" {
   filename         = "processor_lambda.zip"
