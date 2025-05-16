55555706c7b60f119ac6ba0a8945baee0823d679
iamrosada0 <luisrosada@outlook.com>
2025-05-16 17:01:32 +0100

    feat: configure AWS SDK with LocalStack endpoint and anonymous credentials

--- cmd/processor/main.go
+++ cmd/processor/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
 type TelemetryData struct {
 	DeviceID  string  `json:"device_id"`
 	Timestamp string  `json:"timestamp"`
@@ -14,4 +23,19 @@ type TelemetryData struct {
 type SQSPayload struct {
 	Type string        `json:"type"`
 	Data TelemetryData `json:"data"`
+}
+
+func HandleRequest(ctx context.Context, event events.SQSEvent) (string, error) {
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion("us-west-2"),
+		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
+			func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: os.Getenv("LOCALSTACK_ENDPOINT")}, nil
+			})),
+		config.WithCredentialsProvider(aws.AnonymousCredentials{}),
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to load AWS config: %v", err)
+	}
+
 }
