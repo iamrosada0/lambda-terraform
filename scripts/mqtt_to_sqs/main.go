a5c4b9c1bbddddd926d7e155eadde93c6e9efc65
iamrosada0 <luisrosada@outlook.com>
2025-05-17 07:46:26 +0100

    feat: Payload represents the structure of the MQTT and SQS message

---
+++ scripts/mqtt_to_sqs/main.go
@@ -0,0 +1,15 @@
+package main
+
+type Payload struct {
+	Type string `json:"type"`
+	Data struct {
+		DeviceID  string  `json:"device_id"`
+		Timestamp string  `json:"timestamp"`
+		X         float64 `json:"x,omitempty"`
+		Y         float64 `json:"y,omitempty"`
+		Z         float64 `json:"z,omitempty"`
+		Latitude  float64 `json:"latitude,omitempty"`
+		Longitude float64 `json:"longitude,omitempty"`
+		Image     string  `json:"image,omitempty"`
+	} `json:"data"`
+}
