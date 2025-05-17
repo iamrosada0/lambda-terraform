85d90738c5f01245f6c8edabb8aa57516379f6d7
iamrosada0 <luisrosada@outlook.com>
2025-05-17 07:48:06 +0100

    feat: Configure SQS client

--- scripts/mqtt_to_sqs/main.go
+++ scripts/mqtt_to_sqs/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
 type Payload struct {
 	Type string `json:"type"`
 	Data struct {
@@ -12,4 +24,30 @@ type Payload struct {
 		Longitude float64 `json:"longitude,omitempty"`
 		Image     string  `json:"image,omitempty"`
 	} `json:"data"`
+}
+
+func main() {
+	opts := mqtt.NewClientOptions().
+		AddBroker("tcp://localhost:1883").
+		SetClientID("mqtt-to-sqs").
+		SetConnectRetry(true).
+		SetConnectRetryInterval(5 * time.Second)
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		fmt.Printf("Error connecting to Mosquitto: %v\n", token.Error())
+		os.Exit(1)
+	}
+	defer client.Disconnect(250)
+
+	cfg, err := config.LoadDefaultConfig(context.Background(),
+		config.WithRegion(os.Getenv("AWS_DEFAULT_REGION")),
+		config.WithCredentialsProvider(aws.AnonymousCredentials{}),
+	)
+	if err != nil {
+		fmt.Printf("Error loading AWS config: %v\n", err)
+		os.Exit(1)
+	}
+	sqsClient := sqs.NewFromConfig(cfg, func(o *sqs.Options) {
+		o.BaseEndpoint = aws.String(os.Getenv("LOCALSTACK_ENDPOINT"))
+	})
 }
